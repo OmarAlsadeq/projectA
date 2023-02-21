6cdaa54ec197d63049bf4ae6098e77b23fb7120d
Daniel Ratzlaff <Asylumgames39@gmail.com>
2023-02-20 18:20:31 -0600

    added additional methods for unused calls

--- src/main/java/Controllers/AuthenticationController.java
+++ src/main/java/Controllers/AuthenticationController.java
@@ -1,7 +1,13 @@
 package Controllers;
 
+import Models.User;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class AuthenticationController {
+    public User getUserFromSession(HttpSession session) {
+        return null;
+    }
 }
